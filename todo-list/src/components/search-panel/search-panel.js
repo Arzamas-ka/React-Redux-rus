4057d55e0eabe416247d6633f68ee09e876a89f3
Liudmila Arzamasova <signum.ru@gmail.com>
2019-08-18 18:44:48 +0300

    created sceleton todo-list

---
+++ todo-list/src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <input className="form-control search-input" type="text" placeholder="type to search"></input>
+  );
+};
+
+export default SearchPanel;
+
