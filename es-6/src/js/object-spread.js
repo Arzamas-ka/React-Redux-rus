6c26b0c1332791d58bc1744ab2b50d65574a1394
Liudmila Arzamasova <signum.ru@gmail.com>
2019-08-20 23:42:33 +0300

    lerned ES6

---
+++ es-6/src/js/object-spread.js
@@ -0,0 +1,17 @@
+const def = {
+  host: 'local',
+  dName: 'blog',
+  user: 'admin', 
+};
+
+const opt = {
+  user: 'peter',
+  pass: 123,
+};
+
+const defOpt = Object.assign({}, def. opt);
+console.log(defOpt);
+
+const defOptions = {...def, ...opt };
+console.log(defOptions);
+
