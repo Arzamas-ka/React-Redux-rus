bde396b2647652667fac59930c3f5b9349effe6c
Liudmila Arzamasova <signum.ru@gmail.com>
2019-09-11 18:53:46 +0300

    lifecycle of components

---
+++ star-db/src/components/people-page/index.js
@@ -0,0 +1,3 @@
+import PeoplePage from '../people-page/people-page';
+
+export default PeoplePage;
--- star-db/src/components/person-details/person-details.js
+++ star-db/src/components/person-details/person-details.js
@@ -1,33 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './person-details.css';
+import SwapiService from "../../services/swapi-service";
+import ErrorButton from '../error-button/error-button';
+import "./person-details.css";
 
 export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    person: null
+  };
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      return this.updatePerson();
+    }
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService
+      .getPerson(personId)
+      .then((person) => {
+        this.setState({ person });
+    });
+  }
 
   render() {
-    return (
-      <div className='person-details card'>
-        <img className='person-image' src='https://starwars-visualguide.com/assets/img/characters/3.jpg' alt='starwars'>
-        </img>
 
-        <div className='card-body'>
-          <h4>R2-D2</h4>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <span className='term'>Gender</span>
-              <span>male</span>
+    if (!this.state.person) {
+      return <span>Selected a person from a list</span>;
+    }
+
+    const { id, name, gender, birthYear, eyeColor } = this.state.person;
+
+    return (
+      <div className="person-details card">
+        <img
+          className="person-image"
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt="starwars"
+        />
+
+        <div className="card-body">
+          <h4>{name} {this.props.personId}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Gender</span>
+              <span>{gender}</span>
             </li>
-            <li className='list-group-item'>
-              <span className='term'>Birth Year</span>
-              <span>male</span>
+            <li className="list-group-item">
+              <span className="term">Birth Year</span>
+              <span>{birthYear}</span>
             </li>
-            <li className='list-group-item'>
-              <span className='term'>Eye Color</span>
-              <span>male</span>
+            <li className="list-group-item">
+              <span className="term">Eye Color</span>
+              <span style={{ color: 'red' }}>{eyeColor}</span>
+            </li>
+            <li className="list-group-item">
+              <ErrorButton styles={{ margin: 0 }} />
             </li>
           </ul>
         </div>
       </div>
-    )
+    );
   }
 }
